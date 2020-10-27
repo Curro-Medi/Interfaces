684d00009d4292696f248a74232c1988c996d4e8
Curro <francisco.mediavilla.garcia@alumnos.fesac.es>
2020-10-27 12:34:49 +0100

    Seguimos

---
+++ Practica_4_teatro/Practica_4_teatro/Asientos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_4_teatro
+{
+    class Asientos
+    {
+        private int fila;
+        private int columna;
+        private Boolean estado;
+
+    }
+}
