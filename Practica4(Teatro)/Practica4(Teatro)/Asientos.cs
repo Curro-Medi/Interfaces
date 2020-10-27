3be8aac2f06a99f9235ed476ba9106ae94aa543c
Curro <francisco.mediavilla.garcia@alumnos.fesac.es>
2020-10-27 12:22:12 +0100

    kk

---
+++ Practica4(Teatro)/Practica4(Teatro)/Asientos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4_Teatro_
+{
+    class Asientos
+    {
+
+        private int fila;
+        private int columna;
+        private Boolean estado;
+
+
+    }
+}
---
+++ Practica4(Teatro)/Practica4(Teatro)/Sala.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4_Teatro_
+{
+    class Sala
+    {
+
+        private String nombrevento;
+        private String fecha;
+        private String hora;
+        private List<Asientos> listaasientos;
+
+
+        public void versala()
+        {
+
+        }
+
+        public void reservarasiento()
+        {
+
+        }
+
+        public void comprarasiento()
+        {
+
+        }
+
+    }
+}
