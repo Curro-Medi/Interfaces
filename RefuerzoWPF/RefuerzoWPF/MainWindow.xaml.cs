5a562f0a8de45e3d886c402f5d5d79c7b541fb1e
Curro Mediavilla <59694404+Curro-Medi@users.noreply.github.com>
2021-02-09 12:33:00 +0100

    Add files via upload

---
+++ RefuerzoWPF/RefuerzoWPF/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RefuerzoWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static bool sarandonga = true;
+        List<String> versosperversos = new List<string>();
+        List<String> listaperversa = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(List<String> listaperversa, List<String> versosperversos)
+        {
+            InitializeComponent();
+            this.listaperversa = listaperversa;
+            this.versosperversos = versosperversos;
+
+            foreach (String g in listaperversa)
+            {
+                Lista.Items.Add(g);
+
+            }
+
+            foreach (String g in versosperversos)
+            {
+                Combobo.Items.Add(g);
+
+            }
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (sarandonga)
+            {
+                Lista.Items.Add(Textbo.Text);
+                listaperversa.Add(Textbo.Text);
+                Textbo.Clear();
+
+            }
+            else
+            {
+                Combobo.Items.Add(Textbo.Text);
+                versosperversos.Add(Textbo.Text);
+                Textbo.Clear();
+
+            }
+
+        }
+
+        private void btnConmutar_Click(object sender, EventArgs e)
+        {
+            if (sarandonga)
+            {
+                sarandonga = false;
+            }
+            else
+            {
+                sarandonga = true;
+            }
+
+        }
+        private void btnWPF2_Click(object sender, RoutedEventArgs e)
+        {
+            WPF2 f2 = new WPF2(listaperversa, versosperversos);
+            f2.Show();
+            this.Hide();
+        }
+
+        private void Textbo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RefuerzoWPF/RefuerzoWPF/WPF2.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RefuerzoWPF
+{
+    /// <summary>
+    /// Lógica de interacción para WPF2.xaml
+    /// </summary>
+    public partial class WPF2 : Window
+    {
+        List<String> listaperversa;
+        List<String> comboperverso;
+        static bool sarandonga = true;
+
+        public WPF2()
+        {
+            InitializeComponent();
+        }
+
+        public WPF2(List<String> listaperversa, List<String> comboperverso)
+        {
+            InitializeComponent();
+
+            this.listaperversa = listaperversa;
+            this.comboperverso = comboperverso;
+
+            foreach (String g in listaperversa)
+            {
+                Lista2.Items.Add(g);
+
+            }
+
+            foreach (String g in comboperverso)
+            {
+                Combobo2.Items.Add(g);
+
+            }
+        }
+
+        private void btnWPF1_Click(object sender, RoutedEventArgs e)
+        {
+
+            MainWindow f1 = new MainWindow(listaperversa, comboperverso);
+            this.Hide();
+            f1.Show();
+
+        }
+
+        private void btnAgregar2_Click(object sender, RoutedEventArgs e)
+        {
+            if (sarandonga)
+            {
+                Lista2.Items.Add(Textbo2.Text);
+                listaperversa.Add(Textbo2.Text);
+                Textbo2.Clear();
+            }
+            else
+            {
+                Combobo2.Items.Add(Textbo2.Text);
+                comboperverso.Add(Textbo2.Text);
+                Textbo2.Clear();
+            }
+        }
+
+        private void btnConmutar2_Click(object sender, RoutedEventArgs e)
+        {
+            if (sarandonga)
+            {
+                sarandonga = false;
+            }
+            else
+            {
+                sarandonga = true;
+            }
+
+        }
+    }
+}
